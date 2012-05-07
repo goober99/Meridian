364051bf2734e423d3e2fab44f288107a8a7b110
Matthew D. Miller <matthewdmiller@swingthesickle.com>
2012-05-07 17:07:59 -0500

    Added test for add-point function.

--- t/timeline.scm
+++ t/timeline.scm
@@ -4,3 +4,20 @@
 
 (require rackunit)              ; Import Test Framework
 (require "../lib/timeline.scm") ; Import file to test
+
+(test-case
+  "Timeline data"
+  ;; Try adding an event to the timeline:
+  (check-equal? (add-point  "1941" "Citizen Kane")
+                (list (list "1941" "Citizen Kane")))
+                
+  ;; Add an event that should be sorted after prev event:
+  (check-equal? (add-point  "1946" "The Stranger")
+                (list (list "1941" "Citizen Kane")
+                      (list "1946" "The Stranger")))
+  
+  ;; Add an event that should be sorted between other two:
+  (check-equal? (add-point  "1942" "The Magnificent Ambersons")
+                (list (list "1941" "Citizen Kane")
+                      (list "1942" "The Magnificent Ambersons")
+                      (list "1946" "The Stranger"))))
