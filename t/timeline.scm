8330f3c6008bd775548f3f498b5f0e0e1ca78713
Matthew D. Miller <matthewdmiller@swingthesickle.com>
2012-05-07 18:17:14 -0500

    Added tests for unimplemented functions (modify-date, edit-desc, remove-point).

--- t/timeline.scm
+++ t/timeline.scm
@@ -6,8 +6,9 @@
 (require "../lib/timeline.scm") ; Import file to test
 
 (test-case
-  "Timeline data"
-  ;; Try adding an event to the timeline:
+  "Adding events to timeline"
+  
+  ;; Add an event to the timeline:
   (check-equal? (add-point  "1941" "Citizen Kane")
                 (list (list "1941" "Citizen Kane")))
                 
@@ -21,3 +22,29 @@
                 (list (list "1941" "Citizen Kane")
                       (list "1942" "The Magnificent Ambersons")
                       (list "1946" "The Stranger"))))
+
+;(test-case
+;  "Modifying events on timeline"
+;  ;; Add Orson Welles first movie with wrong date and misspelled:
+;  (add-point "1940" "Citizen Cane")
+;  
+;  ;; Fix date:
+;  (check-equal? (modify-date "1940" "1941")
+;                (list (list  "1941" "Citizen Cane")))
+;  
+;  ;; Correct misspelling:
+;  (check-equal? (edit-desc  "1941" "Citizen Kane")
+;                (list (list "1941" "Citizen Kane"))))
+;
+;(test-case
+;  "Removing events from timeline"
+;  ;; Add a couple movies to timeline:
+;  (add-point "1941" "Citizen Kane")
+;  (add-point "1967" "Casino Royale")
+;  
+;  ;; Remove movie not directed by Orson Welles:
+;  (check-equal? (remove-point "1967")
+;                (list (list "1941" "Citizen Kane")))
+;                
+;  ;; Removing the only event should result in an empty list:
+;  (check-equal? (remove-point "1941") '()))
